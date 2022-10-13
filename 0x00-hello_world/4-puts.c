c863c55fdae3706fcb13a55cd1d72704d9ebd04d
Adebanjo Oluwasegun <crownfittt@gmail.com>
2022-10-13 15:22:39 -0700

    Taks 3 4 5 and 6

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,21 @@
+nclude <stdio.h>
+
+/**
+ *
+ *  *main - entry point
+ *
+ *   *
+ *
+ *    *Return: 0 after printing the function
+ *
+ *     */
+
+int main(void)
+
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+
+}
