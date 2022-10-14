6797ada718b0427200730436f7482376b24a520c
Adebanjo Oluwasegun <crownfittt@gmail.com>
2022-10-14 15:34:16 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
-
-
 /**
- *main - entry point
  *
- *    *Return: Always  0 (Success)
+ *  * main - Entry point
+ *
+ *   *C program that prints exactly "Programming is like building a multilingual puzzle"
+ *
+ *    * Return: Always 0 (Success)
  *
  *     */
+
 int main(void)
+
 {
 
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-
+	    return (0);
+puts("\"Programming is like building a multilingual puzzle");
 }
