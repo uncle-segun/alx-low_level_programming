44488ce97f1d4d8c01cdeaab4f609d053f22c126
Adebanjo Oluwasegun <crownfittt@gmail.com>
2022-10-17 08:32:41 -0700

    Alphabets a - z in lowercase

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- *  * main - Entry point
- *   * Description: prints the alphabet in lowercase\n
+ *   main - Entry point
+ *  Description: prints the alphabet in lowercase\n
  *    * Return: Always 0 (success)
- *     */
+ */
 int main(void)
 {
 	int ch;
