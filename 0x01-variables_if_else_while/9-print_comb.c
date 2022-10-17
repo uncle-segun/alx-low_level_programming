879af43ba0efc4d1221bcd1b27c4187442961083
Adebanjo Oluwasegun <crownfittt@gmail.com>
2022-10-17 16:41:57 -0700

    Print Single Digit Numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ * Descrption: Print numbers
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i= '0' i <= '9' ; i++)
+	{
+		putchar(i);
+
+		if( i != '9');
+		{ 
+			puthcar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
