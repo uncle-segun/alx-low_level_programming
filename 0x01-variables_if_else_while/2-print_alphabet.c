89fe135eeda165695bd019690d134f2f6d9172ab
Adebanjo Oluwasegun <crownfittt@gmail.com>
2022-10-17 09:10:48 -0700

    Alphabet a to z lowercase

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,11 +3,12 @@
 /**
  *   main - Entry point
  *  Description: prints the alphabet in lowercase\n
- *    * Return: Always 0 (success)
+ * Return: Always 0 (success)
  */
 int main(void)
 {
 	int ch;
+
 	for (ch = 'a'; ch <= 'z'; ch++)
 	{
 	putchar(ch);
