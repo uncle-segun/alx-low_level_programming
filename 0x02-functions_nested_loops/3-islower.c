8adaa650183d42eab7afc3973f75768c9599fed6
Adebanjo Oluwasegun <crownfittt@gmail.com>
2022-10-18 16:22:00 -0700

    Lowercase of alphabets

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main"
+/**
+* _islower - Entry point
+*  Description: checks is a character is lowercase
+*
+* c: the integer value it receives
+*
+*  Return: 1 if true. 0 if false.
+*
+*/
+int _islower(int c)
+{
+
+	int i = 'a';
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(c);
+	}
+
+		_putchar('\n');
+}		
