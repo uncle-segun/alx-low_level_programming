e01cdeafe0dee015fe81b25f6eab263e79d49eec
Adebanjo Oluwasegun <crownfittt@gmail.com>
2022-10-17 18:11:38 -0700

    Single digit Numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,27 +1,29 @@
 #include <stdio.h>
+
 #include <unistd.h>
+
 /**
- * main - Entry point
- * Descrption: Print numbers
- * Return: Always 0 (success)
- */
+ *
+ *  * main - Entry point
+ *
+ *   * Description: prints numbers
+ *
+ *    * Return: Always 0 (success)
+ *
+ *     */
+
 int main(void)
+
 {
 	int i;
-
-	for (i= '0'; i <= '9' ; i++)
+	for (i = '0'; i <= '9' ; i++)
 	{
-		putchar(i);
-
-		if( i == '9');
-		{
-			break;
-		}
-		else{
-			putchar( ',');
-			putchar(' ');
-		}
+	putchar(i);
+	if (i != '9')
+	{
+	putchar(',');																				putchar(' ');																}
 	}
 	putchar('\n');
 	return (0);
+
 }
