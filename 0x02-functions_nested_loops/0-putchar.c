26bed66965e77573e60ac785f2804937dd5bbcf8
Adebanjo Oluwasegun <crownfittt@gmail.com>
2022-10-18 14:42:53 -0700

    Print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * Description - A Program that print _putchar
+ * Return : Alway (0) Suscess
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}	
