4f0340e731e9e014cfc885a07a74ee381f460084
Adebanjo Oluwasegun <crownfittt@gmail.com>
2022-10-19 02:04:19 -0700

    Print sign if 0 1 or -1

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,26 +8,24 @@
 int print_sign(int n)
 
 {
-	if (n>0)
+	if (n > 0)
 	{
-		putchar ('+');
+		_putchar('+');
 		return (1);
 	}
-	else if
+	else if (n == 0)
 	{
-		 (n==0)
-		putchar ('0');
+		_putchar('0');
 		 return (0);
 	}
-	else if 
+	else if  (n < 0)
 	{
-		(n<0)
-		putchar ('-');
+		_putchar('-');
 		return (-1);
 	}
 	else
 	{	
-		putchar ('-');
-		return  (/);
+		_putchar('-');
+		return  ('/');
 	}
 }
