9a37730d300ea6e40a8d01a5faa59220cfaa57fb
Adebanjo Oluwasegun <crownfittt@gmail.com>
2022-10-19 01:52:47 -0700

    Return 1 0 or -1

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * Entry Point - Function That print Number sign
+ * Description - This program will print number signs
+ *
+ * Return - either 1 or 0 or -1
+ */
+int print_sign(int n)
+
+{
+	if (n>0)
+	{
+		putchar ('+');
+		return (1);
+	}
+	else if
+	{
+		 (n==0)
+		putchar ('0');
+		 return (0);
+	}
+	else if 
+	{
+		(n<0)
+		putchar ('-');
+		return (-1);
+	}
+	else
+	{	
+		putchar ('-');
+		return  (/);
+	}
+}
