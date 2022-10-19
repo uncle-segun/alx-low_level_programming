1cc122ada8779ae48877ce42f4ed584fa0e7a6ef
Adebanjo Oluwasegun <crownfittt@gmail.com>
2022-10-19 02:26:41 -0700

    Absolute value of n from 0

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,14 +1,13 @@
 #include "main.h"
-/**_abs - calculate absolute value of the number from zero.
-*
-* @i: parameter.
+/** Entry Point - calculate absolute value of the number from zero.
+*Description - _abs absolute value 
 *
 *  Return: absolute value of input.
 */
 int _abs(int i)
 
 {
-	if (i < 0 )
+	if (i < 0)
 		i = i * -1;
 	return (i);
 }
