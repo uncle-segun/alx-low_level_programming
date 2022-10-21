808e866a67b95f1fc4b08651a39f4fc684a59dd5
Adebanjo Oluwasegun <crownfittt@gmail.com>
2022-10-21 16:34:10 -0700

    function that prints out digit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * isupper - checks for uppercase character
+ * @c: the character to be checked
+ * Description - funcion _isupper.c
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+
+int _isupper(int c)
+
+{
+
+		return (c >= 'A' && c <= 'Z');
+
+}
